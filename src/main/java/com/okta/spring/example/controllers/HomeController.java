449202b67d07ffb1a868c931f457ea348747cda1
Sushant <sushant.lahoti2005@gmail.com>
2021-01-06 21:27:07 -0600

    CandB user session

---
+++ src/main/java/com/okta/spring/example/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.okta.spring.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/token")
+    public String token() {
+        return "token";
+    }
+
+
+}
